a532ddd85c18c6d138237dc559157733b1066367
Etienne Klepal <etienne.klepal@gmail.com>
2022-12-25 23:05:33 +0100

    part 1 of day three done

--- day-03/day_03_01.rb
+++ day-03/day_03_01.rb
@@ -7,8 +7,16 @@ INPUT = open('input.txt').read
 array = INPUT.dup.split(/\n/)
 splited = array.map { |e| [e[0...e.size/2], e[e.size/2...e.size]]}
 
-reduced = splited.each do |pair|
-  pair.first.each_char { |char| pair.last.include? char }
+def find_double(pair)
+  double = ''
+  pair.first.each_char do |char|
+    double = char if pair.last.include? char
+  end
+  return double
 end
 
-p array
+reduced = splited.map do |pair|
+  1 + array_letters.index(find_double(pair))
+end
+
+p reduced.sum
