473fc229f8c2adad9c342ce5eb524d03440c27e5
Etienne Klepal <etienne.klepal@gmail.com>
2022-12-28 09:55:19 +0100

    done with first part of day 8

--- day-08/day_08_01.rb
+++ day-08/day_08_01.rb
@@ -2,19 +2,19 @@
 
 INPUT = IO.readlines('input.txt', chomp: true)
 
-p INPUT
-array_right = []
-INPUT.dup.each do |el|
-  a = []
-  el.each_char.with_index do |char|
-    a << el[0]
-    index = 1
-    while el[index].to_i > el[index - 1].to_i && index <= el.size do
-      a << char
-      index += 1
+counter = 0
+INPUT.dup.each_with_index do |row, row_i|
+  row.each_char.with_index do |char, char_i|
+    if row_i.zero? ||
+       INPUT[0...row_i].all? { |r| char.to_i > r[char_i].to_i } ||
+       char_i == row.size - 1 ||
+       row[(char_i + 1)...row.size].each_char.all? { |c| char.to_i > c.to_i } ||
+       row_i == INPUT.size - 1 ||
+       INPUT[(row_i + 1)...INPUT.size].all? { |r| char.to_i > r[char_i].to_i } ||
+       char_i.zero? ||
+       row[0...char_i].each_char.all? { |c| char.to_i > c.to_i }
+      counter += 1
     end
   end
-  array_right << a
 end
-
-p array_right
+p counter
