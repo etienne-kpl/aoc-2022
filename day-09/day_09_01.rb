49479a73cc06c77f4f4795adf3dce94ea90bb3c6
Etienne Klepal <etienne.klepal@gmail.com>
2022-12-29 16:30:10 +0100

    progressed a bit on day 9

--- day-09/day_09_01.rb
+++ day-09/day_09_01.rb
@@ -2,9 +2,25 @@
 
 INPUT = IO.readlines('input.txt', chomp: true)
 
-array = INPUT.map do |el|
+commands = INPUT.map do |el|
   regexp = /(\w) (\d+)/
   [regexp.match(el)[1], regexp.match(el)[2].to_i]
 end
 
-p array
+x = 0
+y = 0
+
+visited_positions = [{ x: x, y: y }]
+commands.map do |command|
+  case command.first
+  when 'U' then y += 1
+  when 'R' then x += 1
+  when 'D' then y -= 1
+  when 'L' then x -= 1
+  end
+  visited_positions << { x: x, y: y }
+end
+
+p x
+p y
+p visited_positions.uniq.size
