1186db41a2a5682d688910dbc5c8b11ebeaa04af
Etienne Klepal <etienne.klepal@gmail.com>
2022-12-31 13:59:21 +0100

    a lot of refacto but finally solved day 9

--- day-09/day_09_02.rb
+++ day-09/day_09_02.rb
@@ -6,61 +6,43 @@ INPUT = IO.readlines('input.txt', chomp: true)
 
 # INPUT = ['R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2']
 
-commands = INPUT.map do |el|
-  regexp = /(\w) (\d+)/
-  [regexp.match(el)[1], regexp.match(el)[2].to_i]
-end
+commands = INPUT.map(&:split).each { |el| el[1] = el[1].to_i }
 
 x = 0
 y = 0
 
-head_positions = [{ x: x, y: y }]
-commands.map do |command|
-  case command.first
-  when 'U'
-    command.last.times do
-      y += 1
-      head_positions << { x: x, y: y }
+head_positions = [[x, y]]
+commands.each do |command|
+  command[1].times do
+    case command[0]
+    when 'U' then y += 1
+    when 'R' then x += 1
+    when 'D' then y -= 1
+    when 'L' then x -= 1
     end
-  when 'R'
-    command.last.times do
-      x += 1
-      head_positions << { x: x, y: y }
-    end
-  when 'D'
-    command.last.times do
-      y -= 1
-      head_positions << { x: x, y: y }
-    end
-  when 'L'
-    command.last.times do
-      x -= 1
-      head_positions << { x: x, y: y }
-    end
+    head_positions << [x, y]
   end
 end
-
 nodes_array = [head_positions]
 
 9.times do
   current_node = nodes_array.last.clone.map(&:clone)
   nodes_array << current_node
 
-  current_node.map.with_index do |el, i|
-    break if current_node[i + 1].nil?
+  current_node.each_cons(2) do |cur, nxt|
+    if (nxt[0] - cur[0]).abs <= 1 && (nxt[1] - cur[1]).abs <= 1
+      nxt[0] = cur[0]
+      nxt[1] = cur[1]
+    end
 
-    if current_node[i + 1][:x] - el[:x] == 2
-      current_node[i + 1][:x] = el[:x] + 1
-    elsif current_node[i + 1][:x] - el[:x] == -2
-      current_node[i + 1][:x] = el[:x] - 1
-    elsif current_node[i + 1][:y] - el[:y] == 2
-      current_node[i + 1][:y] = el[:y] + 1
-    elsif current_node[i + 1][:y] - el[:y] == -2
-      current_node[i + 1][:y] = el[:y] - 1
-    else
-      current_node[i + 1] = el
+    if (nxt[0] - cur[0]).abs >= 2
+      nxt[0] = cur[0] + ((nxt[0] - cur[0]).positive? ? 1 : -1)
+    end
+
+    if (nxt[1] - cur[1]).abs >= 2
+      nxt[1] = cur[1] + ((nxt[1] - cur[1]).positive? ? 1 : -1)
     end
   end
 end
 
-p nodes_array.last.flatten.uniq.size
+nodes_array.each { |el| p el.uniq.size }
