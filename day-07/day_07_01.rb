57e3880ef15639d2cb16452faceefdbda7f90028
Etienne Klepal <etienne.klepal@gmail.com>
2023-01-02 23:08:50 +0100

    some progress on the data

--- day-07/day_07_01.rb
+++ day-07/day_07_01.rb
@@ -1,3 +1,5 @@
+require 'tree'
 # frozen_string_literal: true
 
-INPUT = IO.readlines('input.txt', chomp: true)
+INPUT = IO.readlines('test_input.txt', chomp: true)
+# INPUT = IO.readlines('input.txt', chomp: true)
--- day-11/day_11_01.rb
+++ day-11/day_11_01.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 # INPUT = IO.readlines('input.txt', chomp: true)
-INPUT = IO.readlines('test_input.txt', chomp: true)
+INPUT = open('test_input.txt').read
 
+commands = INPUT.dup.split(/\n\n/)
+commands.map! { |el| el.split(/\n/) }
+commands.each {|el| el.map! { |el| el.scan(/\d+/)} }
+p commands
 #divide by three
+
+class Monkey
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @items = attributes[:items] || []
+    @operation = attributes[:operation]
+    @test = attributes[:test]
+    @monkey_if_true = attributes[:monkey_if_true]
+    @monkey_if_false = attributes[:monkey_if_false]
+  end
+end
