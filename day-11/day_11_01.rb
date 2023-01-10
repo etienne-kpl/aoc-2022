4ddf2d334426d38309a9b9ce916d98e3a0077d62
Etienne Klepal <etienne.klepal@gmail.com>
2023-01-10 17:15:10 +0100

    made good progress on the class

--- day-11/day_11_01.rb
+++ day-11/day_11_01.rb
@@ -8,15 +8,25 @@ class Monkey
   @monkeys = {}
   def self.parse(monkey_data)
     n, i, o, t, tr, fa = monkey_data.split(/\n/)
+    n = /(\d+)/.match(n)[1].to_i
+    i = i.scan(/\d+/).map(&:to_i)
+    o = o.split('= ').last
+    t = /(\d+)/.match(t)[1].to_i
+    tr = /(\d+)/.match(tr)[1].to_i
+    fa = /(\d+)/.match(fa)[1].to_i
+    @monkeys[n] = Monkey.new(i, o, t, tr, fa)
   end
 
-  def initialize(attributes = {})
-    @id = attributes[:id]
-    @items = attributes[:items] || []
-    @operation = attributes[:operation]
-    @test = attributes[:test]
-    @monkey_if_true = attributes[:monkey_if_true]
-    @monkey_if_false = attributes[:monkey_if_false]
+  def initialize(i, o, t, tr, fa)
+    @items = i || []
+    @operation = o
+    @test = t
+    @monkey_if_true = tr
+    @monkey_if_false = fa
+  end
+
+  def self.all
+    @monkeys
   end
 end
 
@@ -24,9 +34,11 @@ INPUT.dup.split(/\n\n/).each do |monkey_data|
   Monkey.parse(monkey_data)
 end
 
-Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
+p Monkey.all
+
+# Monkey 0:
+#   Starting items: 79, 98
+#   Operation: new = old * 19
+#   Test: divisible by 23
+#     If true: throw to monkey 2
+#     If false: throw to monkey 3
