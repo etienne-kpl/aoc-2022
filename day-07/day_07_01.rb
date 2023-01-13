52d772a31f240fe00b71bee1ffb21fb49b3a4b03
Etienne Klepal <etienne.klepal@gmail.com>
2023-01-13 22:45:42 +0100

    back on day 7 with an idea

--- day-07/day_07_01.rb
+++ day-07/day_07_01.rb
@@ -1,5 +1,17 @@
-require 'tree'
 # frozen_string_literal: true
 
 INPUT = IO.readlines('test_input.txt', chomp: true)
 # INPUT = IO.readlines('input.txt', chomp: true)
+
+commands = INPUT.dup.map(&:split)
+
+commands.each do |command|
+  case command
+  when ['$', 'cd', '..'] then p 'moving up'
+  when ['$', 'cd', dir] then p "moving in #{dir}"
+  when ['$', 'ls'] then p 'listing files'
+  when ['dir', dir_name] then p "encountered dir #{dir_name}"
+  when ['dir', dir_name] then p "encountered dir #{dir_name}"
+  when [size, file_name] then p "encoutered file #{file_name} of size #{size}"
+  end
+end
