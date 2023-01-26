124bb47c8fce882f658239475b5fbeccf212b33e
Etienne Klepal <etienne.klepal@gmail.com>
2023-01-26 21:52:53 +0100

    added an array to store the sizes

--- day-07/day_07_01.rb
+++ day-07/day_07_01.rb
@@ -5,6 +5,9 @@ INPUT = IO.readlines('test_input.txt', chomp: true)
 
 commands = INPUT.dup.map(&:split)
 
+# Default value is 0
+# dir_sizes = Hash.new { |h, k| h[k] = 0 }
+dir_sizes = {}
 path = []
 commands.each do |command|
   case command
@@ -19,7 +22,11 @@ commands.each do |command|
   in ['dir', dir_name]
     p "encountered dir #{dir_name}"
   in [size, file_name]
+    dir_sizes[path.dup] = 0 if dir_sizes[path.dup].nil?
+    dir_sizes[path.dup] += size.to_i
     p "encoutered file #{file_name} of size #{size}"
   end
   p path
 end
+
+p dir_sizes
