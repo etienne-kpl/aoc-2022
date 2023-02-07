9662fbf4f00d286e47f3fa9e64a176de0e2778fd
Etienne Klepal <etienne.klepal@gmail.com>
2023-02-07 22:44:36 +0100

    progress on day 12 part 1

--- day-12/day_12_01.rb
+++ day-12/day_12_01.rb
@@ -3,13 +3,24 @@
 # INPUT = IO.readlines('input.txt', chomp: true)
 INPUT = IO.readlines('test_input.txt', chomp: true)
 
-start = []
-goal = []
+alpha = ('a'..'z').to_a
+
+start, goal = {}
 INPUT.each_with_index do |line, index|
   line.each_char.with_index do |c, i|
-    start = [index, i] if c == 'S'
-    goal = [index, i] if c == 'E'
+    start = {val: 'a', x: i, y: index} if c == 'S'
+    goal = {val: 'z', x: i, y: index} if c == 'E'
   end
+end
+
+cur_pos = start
+moves = [cur_pos.dup]
+until cur_pos == goal do
+  if alpha.index(cur_pos[:val]) <= alpha.index(DESTINATION) - 1
+    moves << DESTINATION
+    cur_pos = DESTINATION
+  end
+  break
 end
 
 
