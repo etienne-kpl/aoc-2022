1371e98b3b29919c236b84728da14810b96e24a1
Etienne Klepal <etienne.klepal@gmail.com>
2023-02-09 11:09:20 +0100

    I think a class will be helpful

--- day-12/day_12_01.rb
+++ day-12/day_12_01.rb
@@ -3,21 +3,21 @@
 # INPUT = IO.readlines('input.txt', chomp: true)
 INPUT = IO.readlines('test_input.txt', chomp: true)
 
-start, goal = {}
+START, GOAL = {}
 INPUT.each_with_index do |line, index|
   line.each_char.with_index do |c, i|
-    start = {val: 'a', x: i, y: index} if c == 'S'
-    goal = {val: 'z', x: i, y: index} if c == 'E'
+    START = {val: 'a', x: i, y: index} if c == 'S'
+    GOAL = {val: 'z', x: i, y: index} if c == 'E'
   end
 end
 
-cur_pos = start
+ALPHA = ('a'..'z').to_a
+cur_pos = START
 moves = [cur_pos.dup]
 
 def check_next(destination)
-  alpha = ('a'..'z').to_a
   !destination[:val].nil? &&
-  alpha.index(cur_pos[:val]) <= alpha.index(destination[:val]) - 1 &&
+  ALPHA.index(cur_pos[:val]) <= ALPHA.index(destination[:val]) - 1 &&
   !moves.include?(destination)
 end
 
@@ -26,40 +26,71 @@ def move_to(destination)
   cur_pos = destination
 end
 
-# Randomize selection ?
-random_dest = %w[top right bottom left]
+class Path
+  def initialize(moves = [])
+    @moves = moves
+    @cur_pos = START
+  end
 
-until cur_pos == goal do
-  # Top
-  destination = {val: INPUT[cur_pos[:y] - 1][cur_pos[:x]], x: cur_pos[:x], y: cur_pos[:y] - 1}
-  if check_next(destination)
-    move_to(destination)
-  else
-    # Right
-    destination = {val: INPUT[cur_pos[:y]][cur_pos[:x] + 1], x: cur_pos[:x] + 1, y: cur_pos[:y]}
-    if check_next(destination)
-      move_to(destination)
-    else
-      # Bottom
-      destination = {val: INPUT[cur_pos[:y] + 1][cur_pos[:x]], x: cur_pos[:x], y: cur_pos[:y] + 1}
-      if check_next(destination)
-        move_to(destination)
-      else
-        # Left
-        destination = {val: INPUT[cur_pos[:y]][cur_pos[:x] - 1], x: cur_pos[:x] - 1, y: cur_pos[:y]}
-        if check_next(destination)
-          move_to(destination)
+  def check_next(destination)
+    !destination[:val].nil? &&
+    ALPHA.index(@cur_pos[:val]) <= ALPHA.index(destination[:val]) - 1 &&
+    !@moves.include?(destination)
+  end
+
+  def move_to(destination)
+    @moves << destination
+    @cur_pos = destination
+  end
+
+  def destroy_dup(destination)
+    Path.all.each do |path|
+      if path.moves.include?(destination)
+        if path.moves.size > @moves
+          path.destroy
         else
-          # If I can't move to anything then we start again from beginning
-          cur_pos = start
-          moves = [cur_pos.dup]
+          self.destroy
         end
       end
     end
   end
-  break
+
+  def pathfind
+    until @cur_pos == GOAL do
+      # Top
+      destination = {val: INPUT[@cur_pos[:y] - 1][@cur_pos[:x]], x: @cur_pos[:x], y: @cur_pos[:y] - 1}
+      if check_next(destination)
+        move_to(destination)
+        destroy_dup(destination)
+      else
+        # Right
+        destination = {val: INPUT[@cur_pos[:y]][@cur_pos[:x] + 1], x: @cur_pos[:x] + 1, y: @cur_pos[:y]}
+        if check_next(destination)
+          destroy_dup(destination)
+          move_to(destination)
+        else
+          # Bottom
+          destination = {val: INPUT[@cur_pos[:y] + 1][@cur_pos[:x]], x: @cur_pos[:x], y: @cur_pos[:y] + 1}
+          if check_next(destination)
+            destroy_dup(destination)
+            move_to(destination)
+          else
+            # Left
+            destination = {val: INPUT[@cur_pos[:y]][@cur_pos[:x] - 1], x: @cur_pos[:x] - 1, y: @cur_pos[:y]}
+            if check_next(destination)
+              destroy_dup(destination)
+              move_to(destination)
+            else
+              # If I can't move to anything then we start again from beginning
+              self.destroy
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
-p start
-p goal
+p START
+p GOAL
 p moves
