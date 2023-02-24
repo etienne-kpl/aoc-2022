2b6902afe6cb6ee90ac3e8ef6942f4d45ed4e01f
Etienne Klepal <etienne.klepal@gmail.com>
2023-02-24 17:55:18 +0100

    forgot to remove some INPUT refs and @cur_pos

--- day-12/day_12_01.rb
+++ day-12/day_12_01.rb
@@ -18,37 +18,42 @@ VALUES = INPUT.dup.map.with_index do |line, index|
   end
 end
 
-PATHS = []
+PATHS = [[START.dup]]
+
+def check_next(destination, cur_pos)
+  # Destination value exists, is up to 1 higher than current position and has not been visited yet
+  !destination[:val].nil? &&
+    cur_pos[:val] >= (destination[:val] - 1) &&
+    PATHS.none? { |path| path.include?(destination) }
+end
 
 def pathfind(path)
-  cur_pos = PATHS.empty ? START.dup : path.last
+  cur_pos = path.last
   # Top
   destination = { val: VALUES[cur_pos[:y] - 1][cur_pos[:x]], x: cur_pos[:x], y: cur_pos[:y] - 1 }
-  PATHS << path.dup.append(destination) if check_next(destination) # Create new path with the destination
+  PATHS << path.dup.append(destination) if check_next(destination, cur_pos) # Create new path with the destination
 
   # Right
-  destination = { val: INPUT[@cur_pos[:y]][@cur_pos[:x] + 1], x: @cur_pos[:x] + 1, y: @cur_pos[:y] }
-  PATHS << path.dup.append(destination) if check_next(destination)
+  destination = { val: VALUES[cur_pos[:y]][cur_pos[:x] + 1], x: cur_pos[:x] + 1, y: cur_pos[:y] }
+  PATHS << path.dup.append(destination) if check_next(destination, cur_pos)
 
   # Bottom
-  destination = { val: INPUT[@cur_pos[:y] + 1][@cur_pos[:x]], x: @cur_pos[:x], y: @cur_pos[:y] + 1 }
-  PATHS << path.dup.append(destination) if check_next(destination)
+  destination = { val: VALUES[cur_pos[:y] + 1][cur_pos[:x]], x: cur_pos[:x], y: cur_pos[:y] + 1 }
+  PATHS << path.dup.append(destination) if check_next(destination, cur_pos)
 
   # Left
-  destination = { val: INPUT[@cur_pos[:y]][@cur_pos[:x] - 1], x: @cur_pos[:x] - 1, y: @cur_pos[:y] }
-  PATHS << path.dup.append(destination) if check_next(destination)
+  destination = { val: VALUES[cur_pos[:y]][cur_pos[:x] - 1], x: cur_pos[:x] - 1, y: cur_pos[:y] }
+  PATHS << path.dup.append(destination) if check_next(destination, cur_pos)
 
   # This path is destroyed after those 4 steps
   PATHS.delete(path)
 end
 
-def check_next(destination)
-  # Destination value exists, is up to 1 higher than current position and has not been visited yet
-  !destination[:val].nil? &&
-    cur_pos[:val] >= destination[:val] - 1 &&
-    PATHS.none? { |path| path.include?(destination) }
+10.times do
+  PATHS.each { |path| pathfind(path) }
 end
 
-p START
-p GOAL
-p VALUES
+# p START
+# p GOAL
+# p VALUES
+# p PATHS
