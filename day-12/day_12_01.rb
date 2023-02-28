4b5afe98196aa829650868aabff9d0b615c0039c
Etienne Klepal <etienne.klepal@gmail.com>
2023-02-28 22:55:34 +0100

    new sorting

--- day-12/day_12_01.rb
+++ day-12/day_12_01.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-INPUT = IO.readlines('input.txt', chomp: true)
-# INPUT = IO.readlines('test_input.txt', chomp: true)
+# INPUT = IO.readlines('input.txt', chomp: true)
+INPUT = IO.readlines('test_input.txt', chomp: true)
 
 VALUES = INPUT.dup.map.with_index do |line, index|
   line.chars.map.with_index do |c, i|
@@ -21,37 +21,37 @@ end
 PATHS = [[START.dup]]
 
 def check_next(destination, cur_pos)
-  # Destination value exists, is reachable on current position, is inside the grid and has not been visited yet
-  !destination[:val].nil? &&
-    destination[:y] >= 0 &&
-    destination[:x] >= 0 &&
-    cur_pos[:val] >= (destination[:val] - 1) &&
-    PATHS.none? { |path| path.include?(destination) }
+  # Destination is reachable on current position and has not been visited yet
+  cur_pos[:val] >= (destination[:val] - 1) && PATHS.none? { |path| path.include?(destination) }
 end
 
 def pathfind(path)
   cur_pos = path.last
-  priority = [
-    { val: VALUES[cur_pos[:y] - 1][cur_pos[:x]], x: cur_pos[:x], y: cur_pos[:y] - 1 }, # Top
-    { val: VALUES[cur_pos[:y]][cur_pos[:x] + 1], x: cur_pos[:x] + 1, y: cur_pos[:y] }, # Right
-    { val: VALUES[cur_pos[:y]][cur_pos[:x] - 1], x: cur_pos[:x] - 1, y: cur_pos[:y] } # Left
-  ]
-  # Bottom (needs a check because of the [cur_pos[:y] + 1][cur_pos[:x]] -> the first part can be nil)
-  unless cur_pos[:y] + 1 >= VALUES.size
-    priority << { val: VALUES[cur_pos[:y] + 1][cur_pos[:x]], x: cur_pos[:x], y: cur_pos[:y] + 1 }
-  end
+  x = cur_pos[:x]
+  y = cur_pos[:y]
+  priority = []
+  # Top
+  priority << { val: VALUES[y - 1][x], x: x, y: y - 1 } if y.positive?
+  # Right
+  priority << { val: VALUES[y][x + 1], x: x + 1, y: y } if VALUES[y].size > x + 1
+  # Bottom
+  priority << { val: VALUES[y + 1][x], x: x, y: y + 1 } if VALUES.size > y + 1
+  # Left
+  priority << { val: VALUES[y][x - 1], x: x - 1, y: y } if x.positive?
+
   priority.sort_by! do |destination|
-    (GOAL[:x] - destination[:x]).abs + (GOAL[:y] - destination[:y]).abs
+    # Sorted by closest and higher
+    [(GOAL[:x] - destination[:x]).abs + (GOAL[:y] - destination[:y]).abs, cur_pos[:val] - destination[:val]]
   end
   priority.each { |destination| PATHS << path.dup.append(destination) if check_next(destination, cur_pos) }
 
   # This path is destroyed after those 4 steps
   PATHS.delete(path)
-  PATHS.sort_by! { |el| (GOAL[:x] - el.last[:x]).abs + (GOAL[:y] - el.last[:y]).abs }
 end
 
 until PATHS.any? { |path| path.include?(GOAL) }
   PATHS.each { |path| pathfind(path) }
+  # PATHS.sort_by! { |path| (GOAL[:x] - path.last[:x]).abs + (GOAL[:y] - path.last[:y]).abs }
 end
 
 p START
