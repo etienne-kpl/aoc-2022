1e71149357b313ae06dbe9ba477e10bbdcdae734
Etienne Klepal <etienne.klepal@gmail.com>
2023-03-01 12:18:28 +0100

    part 2 of day 12 is done, the key was starting from the top

---
+++ day-12/day_12_02.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+INPUT = IO.readlines('input.txt', chomp: true)
+# INPUT = IO.readlines('test_input.txt', chomp: true)
+
+VALUES = INPUT.dup.map.with_index do |line, index|
+  line.chars.map.with_index do |c, i|
+    case c
+    when 'S'
+      START = { val: 1, x: i, y: index }.freeze
+      1
+    when 'E'
+      GOAL = { val: 26, x: i, y: index }.freeze
+      26
+    else
+      ('a'..'z').to_a.index(c) + 1
+    end
+  end
+end
+
+# Starting from the end will be easier
+PATHS = [[GOAL.dup]]
+
+def check_next(destination, cur_pos)
+  # Destination is reachable on current position and has not been visited yet
+  cur_pos[:val] - destination[:val] <= 1 && PATHS.none? { |path| path.include?(destination) }
+end
+
+def pathfind(path)
+  cur_pos = path.last
+  x = cur_pos[:x]
+  y = cur_pos[:y]
+  destinations = []
+  # Top
+  destinations << { val: VALUES[y - 1][x], x: x, y: y - 1 } if y.positive?
+  # Right
+  destinations << { val: VALUES[y][x + 1], x: x + 1, y: y } if VALUES[y].size > x + 1
+  # Bottom
+  destinations << { val: VALUES[y + 1][x], x: x, y: y + 1 } if VALUES.size > y + 1
+  # Left
+  destinations << { val: VALUES[y][x - 1], x: x - 1, y: y } if x.positive?
+
+  destinations.each { |d| PATHS << path.dup.append(d) if check_next(d, cur_pos) }
+end
+
+until PATHS.any? { |path| path.last[:val] == 1 }
+  # Sorted by shortest, lowest
+  PATHS.sort_by! { |path| [path.size, path.last[:val]] }
+  # Taking the optimal path
+  pathfind(PATHS.first)
+  # Deleting it after the pathfinding to keep it clean
+  PATHS.shift
+end
+
+p START
+p GOAL
+# p VALUES
+score = PATHS.dup.select do |path|
+  path.last[:val] == 1
+end
+p score.map(&:size).min - 1
