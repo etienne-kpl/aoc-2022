4ae67118531e30026ed6c8857279c194431e6eec
Etienne Klepal <etienne.klepal@gmail.com>
2023-03-02 17:56:08 +0100

    input data is ready to use

---
+++ day-13/day_13_01.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require 'json'
+
+INPUT = IO.read('test_input.txt').split(/\n\n/).map do |pair|
+  l, r = pair.split(/\n/)
+  { l: JSON.parse(l), r: JSON.parse(r) }
+  # { l: l, r: r }
+end
+
+p INPUT
