3d3b93884da77c09be4b0fed0eece0d3f529b378
Etienne Klepal <etienne.klepal@gmail.com>
2023-03-07 21:11:40 +0100

    found a lead with the zip method and recursion

--- day-13/day_13_01.rb
+++ day-13/day_13_01.rb
@@ -7,4 +7,16 @@ INPUT = IO.read('test_input.txt').split(/\n\n/).map do |pair|
   # { l: l, r: r }
 end
 
-p INPUT
+def compare(pair)
+  if pair.all? { |el| el.is_a? Array }
+    compare(pair)
+  elsif pair.all? { |el| el.is_a? Integer }
+    !pair.last.nil? && pair.first <= pair.last
+  end
+end
+
+INPUT.each_with_index do |el, i|
+  el[:l].zip(el[:r]).each do |pair|
+
+  end
+end
