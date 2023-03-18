86160ce1b8d437afd9f37ba005dd5dbcc5dfcd0e
Etienne Klepal <etienne.klepal@gmail.com>
2023-03-18 19:12:23 +0100

    solved the endless loop

--- day-13/day_13_01.rb
+++ day-13/day_13_01.rb
@@ -11,17 +11,14 @@ def compare(pair)
   p pair
   if pair.all? { |el| el.is_a? Array }
     pair.first.zip(pair.last).each do |new_pair|
-      case compare(new_pair)
-      when 0
-        break 0
-      when 1
-        break 1
-      end
+      compare(new_pair)
     end
-  elsif pair.all? { |el| el.is_a?(Integer) } && pair.first < pair.last
-    return 0
-  elsif pair.all? { |el| el.is_a?(Integer) } && pair.first > pair.last
-    return 1
+  elsif pair.all? { |el| el.is_a?(Integer) }
+    if pair.first < pair.last
+      0
+    elsif pair.first > pair.last
+      1
+    end
   elsif pair.last.nil?
     return 1
   elsif pair.first.is_a? Integer
@@ -37,7 +34,7 @@ count = 0
 INPUT.each_with_index do |packet, index|
   p count
   p ""
-  packet[:l].zip(packet[:r]).each do |pair|
+  packet[:l].zip(packet[:r]).any? do |pair|
     case compare(pair)
     when 0
       count += (index + 1)
