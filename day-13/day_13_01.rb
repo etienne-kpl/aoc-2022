5e477ae5d653e50cb9437982114d7810d2aef4e5
Etienne Klepal <etienne.klepal@gmail.com>
2023-03-31 23:43:39 +0200

    starting over

--- day-13/day_13_01.rb
+++ day-13/day_13_01.rb
@@ -7,47 +7,45 @@ INPUT = IO.read('test_input.txt').split(/\n\n/).map do |pair|
 end
 
 
+# # Got to rethink that part, so it produces 0 or 1 only
+# def compare(pair)
+#   p pair
+#   if pair.all? { |el| el.is_a? Array }
+#     pair.first.zip(pair.last).any? do |new_pair|
+#       compare(new_pair)
+#     end
+#   elsif pair.all? { |el| el.is_a?(Integer) }
+#     if pair.first < pair.last
+#       return 0
+#     elsif pair.first > pair.last
+#       return 1
+#     end
+#   elsif pair.last.nil?
+#     return 1
+#   elsif pair.first.is_a? Integer
+#     compare([[pair.first], pair.last])
+#   else
+#     compare([pair.first, [pair.last]])
+#   end
+# end
 
+# count = 0
 
-# Got to rethink that part, so it produces 0 or 1 only
-def compare(pair)
-  p pair
-  if pair.all? { |el| el.is_a? Array }
-    pair.first.zip(pair.last).any? do |new_pair|
-      compare(new_pair)
-    end
-  elsif pair.all? { |el| el.is_a?(Integer) }
-    if pair.first < pair.last
-      return 0
-    elsif pair.first > pair.last
-      return 1
-    end
-  elsif pair.last.nil?
-    return 1
-  elsif pair.first.is_a? Integer
-    compare([[pair.first], pair.last])
-  else
-    compare([pair.first, [pair.last]])
-  end
-end
+# # Must run, then break and go to next if 0, break and update if 1, update if none of this happened.
+# INPUT.each_with_index do |packet, index|
+#   p count
+#   p ""
+#   packet[:l].zip(packet[:r]).any? do |pair|
+#     case compare(pair)
+#     when 0
+#       count += (index + 1)
+#       break
+#     when 1
+#       break
+#     else
+#       count += (index + 1)
+#     end
+#   end
+# end
 
-count = 0
-
-# Must run, then break and go to next if 0, break and update if 1, update if none of this happened.
-INPUT.each_with_index do |packet, index|
-  p count
-  p ""
-  packet[:l].zip(packet[:r]).any? do |pair|
-    case compare(pair)
-    when 0
-      count += (index + 1)
-      break
-    when 1
-      break
-    else
-      count += (index + 1)
-    end
-  end
-end
-
-p count
+# p count
