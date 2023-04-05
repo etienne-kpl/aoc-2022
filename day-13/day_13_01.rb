1315085ec11ea0d684d07524d0651ebddc2946f3
Etienne Klepal <etienne.klepal@gmail.com>
2023-04-05 15:59:26 +0200

    got it

--- day-13/day_13_01.rb
+++ day-13/day_13_01.rb
@@ -6,46 +6,46 @@ INPUT = IO.read('test_input.txt').split(/\n\n/).map do |pair|
   { l: JSON.parse(l), r: JSON.parse(r) }
 end
 
+count = 0
 
-# # Got to rethink that part, so it produces 0 or 1 only
-# def compare(pair)
-#   p pair
-#   if pair.all? { |el| el.is_a? Array }
-#     pair.first.zip(pair.last).any? do |new_pair|
-#       compare(new_pair)
-#     end
-#   elsif pair.all? { |el| el.is_a?(Integer) }
-#     if pair.first < pair.last
-#       return 0
-#     elsif pair.first > pair.last
-#       return 1
-#     end
-#   elsif pair.last.nil?
-#     return 1
-#   elsif pair.first.is_a? Integer
-#     compare([[pair.first], pair.last])
-#   else
-#     compare([pair.first, [pair.last]])
-#   end
-# end
+def compare(pair)
+  p pair
+  if pair.all? { |el| el.is_a? Array }
+    pair.first.zip(pair.last).each do |new_pair|
+      case compare(new_pair)
+      when 0
+        break 0
+      when 1
+        break 1
+      end
+    end
+  elsif pair.all? { |el| el.is_a?(Integer) }
+    if pair.first < pair.last
+      0
+    elsif pair.first > pair.last
+      1
+    end
+  elsif pair.last.nil?
+    1
+  elsif pair.first.is_a? Integer
+    compare([[pair.first], pair.last])
+  else
+    compare([pair.first, [pair.last]])
+  end
+end
 
-# count = 0
-
-# # Must run, then break and go to next if 0, break and update if 1, update if none of this happened.
-# INPUT.each_with_index do |packet, index|
-#   p count
-#   p ""
-#   packet[:l].zip(packet[:r]).any? do |pair|
-#     case compare(pair)
-#     when 0
-#       count += (index + 1)
-#       break
-#     when 1
-#       break
-#     else
-#       count += (index + 1)
-#     end
-#   end
-# end
-
-# p count
+INPUT.each_with_index do |packet, index|
+  p "pair ##{index + 1}"
+  count += (index + 1)
+  packet[:l].zip(packet[:r]).each do |pair|
+    case compare(pair)
+    when 0
+      p 'right order'
+      break
+    when 1
+      count -= (index + 1)
+      p 'wrong order'
+      break
+    end
+  end
+end
