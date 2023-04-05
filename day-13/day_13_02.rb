bcb6bb48cc41996250847ba10350cbc4263e4707
Etienne Klepal <etienne.klepal@gmail.com>
2023-04-05 18:31:38 +0200

    completed part 2

--- day-13/day_13_02.rb
+++ day-13/day_13_02.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 require 'json'
 
-INPUT = IO.read('test_input.txt').gsub(/\n+/, ' ').split.map { |el| JSON.parse(el) }
+# I must get rid of all the empty lists, otherwise the flatten will pass through them, hence the gsub
+INPUT = IO.read('input.txt').gsub(/\n+/, ' ').gsub('[]', '[0]').split.map { |el| JSON.parse(el) }
 
 array = INPUT.dup.append([[2]], [[6]])
 array.sort_by! { |el| el.flatten }
-
-p array
 
 a = array.find_index([[2]]) + 1
 b = array.find_index([[6]]) + 1
