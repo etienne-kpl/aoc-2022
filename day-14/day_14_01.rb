d62d15a3bcf0e227058cdfe624d5f45d68167991
Etienne Klepal <etienne.klepal@gmail.com>
2023-04-06 21:53:33 +0200

    first draft fr day 14/1

--- day-14/day_14_01.rb
+++ day-14/day_14_01.rb
@@ -7,4 +7,47 @@ INPUT = IO.readlines('test_input.txt', chomp: true).map do |line|
   end
 end
 
-SAND = {x: 500, y: 0}
+rocks = []
+INPUT.each do |line|
+  line.each_cons(2) do |pair|
+    if pair.first[:x] == pair.last[:x]
+      if pair.first[:y] > pair.last[:y]
+        count = pair.last[:y]
+        until rocks.last == pair.first
+          rocks << {x: pair.first[:x], y: count}
+          count += 1
+        end
+      else
+        count = pair.first[:y]
+        until rocks.last == pair.last
+          rocks << {x: pair.first[:x], y: count}
+          count += 1
+        end
+      end
+    else
+      if pair.first[:x] > pair.last[:x]
+        count = pair.last[:x]
+        until rocks.last == pair.first
+          rocks << {x: count, y: pair.first[:y]}
+          count += 1
+        end
+      else
+        count = pair.first[:y]
+        until rocks.last == pair.last
+          rocks << {x: count, y: pair.first[:y]}
+          count += 1
+        end
+      end
+    end
+  end
+end
+
+SOURCE = {x: 500, y: 0}
+
+sand_loc = []
+p rocks
+p rocks.size
+
+# def produce_sand
+
+# end
