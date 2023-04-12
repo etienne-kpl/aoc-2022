e16e76bbb667f2da9860a7eaf66415a9f4269f71
Etienne Klepal <etienne.klepal@gmail.com>
2023-04-12 23:57:20 +0200

    it works but it is too slow, should not do an include check every step

--- day-14/day_14_02.rb
+++ day-14/day_14_02.rb
@@ -3,22 +3,22 @@
 INPUT = IO.readlines('input.txt', chomp: true).map do |line|
   line.split(' -> ').map do |el|
     x, y = el.split(',')
-    {x: x.to_i, y: y.to_i}
+    { x: x.to_i, y: y.to_i }
   end
 end
 
-source = {x: 500, y: 0}
+source = { x: 500, y: 0 }
 rocks = []
 
 INPUT.each do |line|
   rocks << line.first
   line.each_cons(2) do |pair|
     until rocks.last == pair.last
-      if pair.first[:x] == pair.last[:x]
-        rocks << {x: pair.first[:x], y: (pair.first[:y] < pair.last[:y] ? rocks.last[:y] + 1 : rocks.last[:y] - 1)}
-      else
-        rocks << {x: (pair.first[:x] < pair.last[:x] ? rocks.last[:x] + 1 : rocks.last[:x] - 1), y: pair.first[:y]}
-      end
+      rocks << if pair.first[:x] == pair.last[:x]
+                 { x: pair.first[:x], y: (pair.first[:y] < pair.last[:y] ? rocks.last[:y] + 1 : rocks.last[:y] - 1) }
+               else
+                 { x: (pair.first[:x] < pair.last[:x] ? rocks.last[:x] + 1 : rocks.last[:x] - 1), y: pair.first[:y] }
+               end
     end
   end
 end
@@ -26,19 +26,23 @@ end
 # Some lines might cross, so I get rid of multiple references
 rocks.uniq!
 
-# When an unit of sand will pass that point, it'll means we've reach the abyss
-lowest = rocks.max_by { |el| el[:y]}[:y]
+# When an unit of sand will reach that point, it'll means we've reach the bottom
+# +1 because the rocks are under it
+lowest = rocks.max_by { |el| el[:y] }[:y] + 1
 
 sands = rocks.dup
 
 unit = source.dup
-until unit[:y] >= lowest || sands.include?(source)
-  if !sands.include?({x: unit[:x], y: unit[:y] + 1})
+until sands.include?(source)
+  if unit[:y] == lowest
+    sands << unit
+    unit = source.dup
+  elsif !sands.include?({ x: unit[:x], y: unit[:y] + 1 })
     unit[:y] += 1
-  elsif !sands.include?({x: unit[:x] - 1, y: unit[:y] + 1})
+  elsif !sands.include?({ x: unit[:x] - 1, y: unit[:y] + 1 })
     unit[:x] -= 1
     unit[:y] += 1
-  elsif !sands.include?({x: unit[:x] + 1, y: unit[:y] + 1})
+  elsif !sands.include?({ x: unit[:x] + 1, y: unit[:y] + 1 })
     unit[:x] += 1
     unit[:y] += 1
   else
