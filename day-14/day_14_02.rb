1d8a8e2b382edc2182fa22c9e8824f4b6a80997a
Etienne Klepal <etienne.klepal@gmail.com>
2023-04-14 16:30:56 +0200

    parsed the input in a more efficient way

--- day-14/day_14_02.rb
+++ day-14/day_14_02.rb
@@ -1,90 +1,43 @@
 # frozen_string_literal: true
 
-INPUT = IO.readlines('input.txt', chomp: true).map do |line|
+INPUT = IO.readlines('test_input.txt', chomp: true).map do |line|
   line.split(' -> ').map do |el|
     x, y = el.split(',')
-    [x.to_i, y.to_i]
+    { x: x.to_i, y: y.to_i }
   end
 end
 
-SOURCE = [500, 0].freeze
-rocks = []
+SOURCE = { x: 500, y: 0 }.freeze
+parsed_input = []
 
 INPUT.each do |line|
-  rocks << line.first
+  parsed_input << line.first
   line.each_cons(2) do |pair|
-    until rocks.last == pair.last
-      rocks << if pair.first.first == pair.last.first
-                 [pair.first.first, (pair.first.last < pair.last.last ? rocks.last.last + 1 : rocks.last.last - 1)]
+    until parsed_input.last == pair.last
+      parsed_input << if pair.first[:x] == pair.last[:x]
+                 { x: pair.first[:x], y: (pair.first[:y] < pair.last[:y] ? parsed_input.last[:y] + 1 : parsed_input.last[:y] - 1) }
                else
-                 [(pair.first.first < pair.last.first ? rocks.last.first + 1 : rocks.last.first - 1), pair.first.last]
+                 { x: (pair.first[:x] < pair.last[:x] ? parsed_input.last[:x] + 1 : parsed_input.last[:x] - 1), y: pair.first[:y] }
                end
     end
   end
 end
 
-# Some lines might cross, so I get rid of multiple references
-rocks.uniq!
-
-# When an unit of sand will reach that point, it'll means we've reach the bottom
-# +1 because the rocks are under it
-lowest = rocks.max_by(&:last).last + 1
-
-# I'll store the sands unit in an array containing the rocks
-sands = rocks.dup
-
-unit = SOURCE.dup
-until sands.include?(SOURCE)
-  # find the bottom on the same x axis, without the rocks above the y position
-  bottom = sands.select { |el| el.first == unit.first && el.last > unit.last }.min_by(&:last)
-  unit[1] = bottom.nil? ? lowest : bottom.last - 1
-  if unit.last == lowest
-    sands << unit
-    unit = SOURCE.dup
-  elsif !sands.include?([unit.first - 1, unit.last + 1])
-    unit[0] -= 1
-  elsif !sands.include?([unit.first + 1, unit.last + 1])
-    unit[0] += 1
-  else
-    sands << unit
-    unit = SOURCE.dup
-  end
+rocks = {}
+# Some lines might cross, so I get rid of multiple references and simplify the data
+parsed_input.uniq.each do |el|
+  # If the key returns nil, I assign it with an empty array to store the :y locations
+  rocks[el[:x]] ||= []
+  rocks[el[:x]] << el[:y]
 end
 
-p "lowest point: #{lowest}"
-p "Nb of rocks: #{rocks.size}"
-p "Nb of sands unit: #{sands.size - rocks.size}"
+p rocks
 
-# INPUT = IO.readlines('test_input.txt', chomp: true).map do |line|
-#   line.split(' -> ').map do |el|
-#     x, y = el.split(',')
-#     { x: x.to_i, y: y.to_i }
-#   end
-# end
-
-# SOURCE = { x: 500, y: 0 }.freeze
-# rocks = []
-
-# INPUT.each do |line|
-#   rocks << line.first
-#   line.each_cons(2) do |pair|
-#     until rocks.last == pair.last
-#       rocks << if pair.first[:x] == pair.last[:x]
-#                  { x: pair.first[:x], y: (pair.first[:y] < pair.last[:y] ? rocks.last[:y] + 1 : rocks.last[:y] - 1) }
-#                else
-#                  { x: (pair.first[:x] < pair.last[:x] ? rocks.last[:x] + 1 : rocks.last[:x] - 1), y: pair.first[:y] }
-#                end
-#     end
-#   end
-# end
-
-# # Some lines might cross, so I get rid of multiple references
-# rocks.uniq!
-
-# When an unit of sand will reach that point, it'll means we've reach the bottom
-# +1 because the rocks are under it
+# # When an unit of sand will reach that point, it'll means we've reach the bottom
+# # +1 because the rocks are under it
 # lowest = rocks.max_by { |el| el[:y] }[:y] + 1
 
+# # I store the sands unit in an array containing the rocks
 # sands = rocks.dup
 
 # unit = SOURCE.dup
@@ -110,3 +63,56 @@ p "Nb of sands unit: #{sands.size - rocks.size}"
 # p "lowest point: #{lowest}"
 # p "Nb of rocks: #{rocks.size}"
 # p "Nb of sands unit: #{sands.size - rocks.size}"
+
+
+
+# INPUT = IO.readlines('input.txt', chomp: true).map do |line|
+#   line.split(' -> ').map do |el|
+#     x, y = el.split(',')
+#     [x.to_i, y.to_i]
+#   end
+# end
+
+# SOURCE = [500, 0].freeze
+# rocks = []
+
+# INPUT.each do |line|
+#   rocks << line.first
+#   line.each_cons(2) do |pair|
+#     until rocks.last == pair.last
+#       rocks << if pair.first.first == pair.last.first
+#                  [pair.first.first, (pair.first.last < pair.last.last ? rocks.last.last + 1 : rocks.last.last - 1)]
+#                else
+#                  [(pair.first.first < pair.last.first ? rocks.last.first + 1 : rocks.last.first - 1), pair.first.last]
+#                end
+#     end
+#   end
+# end
+
+# # Some lines might cross, so I get rid of multiple references
+# rocks.uniq!
+
+# # When an unit of sand will reach that point, it'll means we've reach the bottom
+# # +1 because the rocks are under it
+# lowest = rocks.max_by(&:last).last + 1
+
+# # I store the sands unit in an array containing the rocks
+# sands = rocks.dup
+
+# unit = SOURCE.dup
+# until sands.include?(SOURCE)
+#   # find the bottom on the same x axis, without the rocks above the y position
+#   bottom = sands.select { |el| el.first == unit.first && el.last > unit.last }.min_by(&:last)
+#   unit[1] = bottom.nil? ? lowest : bottom.last - 1
+#   if unit.last == lowest
+#     sands << unit
+#     unit = SOURCE.dup
+#   elsif !sands.include?([unit.first - 1, unit.last + 1])
+#     unit[0] -= 1
+#   elsif !sands.include?([unit.first + 1, unit.last + 1])
+#     unit[0] += 1
+#   else
+#     sands << unit
+#     unit = SOURCE.dup
+#   end
+# end
